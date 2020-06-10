a785af086c9a30722eba369e75a251fc375c10e4
Hirak Patel <Hirak Patel>
2020-06-10 12:58:38 -0600

    First Commit

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "../components/Header/Header";
+import Board from "../components/Board/Board";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
